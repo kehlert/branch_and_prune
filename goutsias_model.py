98863f810dd423006a9649c8b7aee159dfede1ae
Kurt Ehlert <kehlert@math.wisc.edu>
2019-08-08 16:44:24 -0400

    fixed the goutsias model, got new figures

--- goutsias_model.py
+++ goutsias_model.py
@@ -15,17 +15,17 @@ class GoutsiasModel():
         #[M, DNA, RNA, D, DNA.D, DNA.2D]
         
         self.S = np.array([[1, -1,  0,  0,  0,  0,  0,  0, -2,  2],
-                           [0,  0,  0,  0, -1,  1,  0,  0,  1,  0],
+                           [0,  0,  0,  0, -1,  1,  0,  0,  0,  0],
                            [0,  0,  1, -1,  0,  0,  0,  0,  0,  0],
-                           [0,  0,  0,  0, -1,  1, -1,  1,  0, -1],
+                           [0,  0,  0,  0, -1,  1, -1,  1,  1, -1],
                            [0,  0,  0,  0,  1, -1, -1,  1,  0,  0],
                            [0,  0,  0,  0,  0,  0,  1, -1,  0,  0]])
         AV = 6.0221415 * 10**8
-        self.rate_constants = np.array([0.043, 0.0007, 0.078, 0.0039, 0.012E9 / AV,
+        self.rate_constants = np.array([0.043, 0.0007, 0.0715, 0.0039, 0.012E9 / AV,
                                         0.4791, 0.00012E9 / AV, 0.8765E-11, 0.05E9 / AV, 0.5])
 
     def get_intensities(self, x):
         temp = np.array([x[2], x[0], x[4], x[2], x[1] * x[3],
-                         x[4], x[3] * x[4], x[5], 0.5 * x[0] * (x[0]-1), x[3] * (x[3]-1)], dtype=np.float64)
+                         x[4], x[3] * x[4], x[5], 0.5 * x[0] * (x[0]-1), x[3]], dtype=np.float64)
         temp = np.maximum(temp, 0)
         return np.multiply(self.rate_constants, temp)
